3125d4ca062c8a3519eea55ec2e702e639eaf7a1
bikash2007 <bikashbhandari600@gmail.com>
2024-08-28 18:37:14 +0545

    teams hidden

--- src/components/HomePage/HomePageFooter/Teams/InstantTeam.js
+++ src/components/HomePage/HomePageFooter/Teams/InstantTeam.js
@@ -8,11 +8,13 @@ const InstantTeam = ({ name, image, post, number, quotes }) => {
   return (
     <div className="py-4  flex flex-col  w-fit h-fit  items-center justify-center border-b border-neutral-400/20 ">
       <div className="flex justify-center relative w-full lg:w-1/3">
-        <img
-          src={image}
-          className="rounded-full w-20 lg:w-32 object-cover emerge"
-          alt={`${name}'s profile`}
-        />
+        {image && (
+          <img
+            src={image}
+            className="rounded-full w-20 lg:w-32 object-cover emerge"
+            alt={`${name}'s profile`}
+          />
+        )}
       </div>
       <div className="flex flex-col relative  justify-center items-center lg:items-start text-white mt-4 lg:mt-0 lg:ml-8 text-center lg:text-left">
         <h1 className="text-xl lg:text-3xl font-semibold mt-2">{name}</h1>
